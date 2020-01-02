615ffb402a33b0c9f2a363cbfa2af88970e68a55
DYbaobao <3295224486@qq.com>
2020-01-02 20:44:20 +0800

    个人任务分配（三种方式）

--- src/main/java/com/test/activiti/k_personalTask/TaskTest.java
+++ src/main/java/com/test/activiti/k_personalTask/TaskTest.java
@@ -2,7 +2,10 @@ package com.test.activiti.k_personalTask;
 
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.repository.Deployment;
@@ -31,15 +34,18 @@ public class TaskTest {
 	@Test
 	public void processDifinitionStratTest(){
 		String processDefinitionKey ="task";
+		//流程变量设置办理人
+		Map<String , Object> variables = new HashMap<String, Object>();
+		variables.put("userId", "小马");
 		ProcessInstance processInstance = processEngine.getRuntimeService()
-		              .startProcessInstanceByKey(processDefinitionKey);
+		             .startProcessInstanceByKey(processDefinitionKey, variables);
 		System.out.println("流程定义ID : " + processInstance.getProcessInstanceId());
 		System.out.println("流程实例ID : " + processInstance.getId());
 	}
 	/**查询本人的任务*/
 	@Test
 	public void findMyPersonTaskTest(){
-		String assignee = "";
+		String assignee = "小马";
 		List<Task> list = processEngine.getTaskService().createTaskQuery().taskAssignee(assignee).list();
 		if (list != null && list.size() > 0) {
 			for (Task task : list) {
@@ -60,7 +66,7 @@ public class TaskTest {
 	/**办理任务*/
 	@Test
 	public void completeTaskTest(){
-		String taskId = "";
+		String taskId = "5905";
 		processEngine.getTaskService().complete(taskId);
 		System.out.println("任务完成! 任务id是:  " + taskId);
 	}
